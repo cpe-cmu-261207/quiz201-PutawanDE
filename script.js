56c13ec2bd116ae228a0cb382835de763adfa3b8
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-07-29 14:19:38 +0700

    Finish paragraph section

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+const search_btn = document.querySelector('#search')
+const paragraph = document.querySelector('#text')
 
 // define more constants and variables here
 let showAuthor = false
-
+let text = ""
 
 btn_toggle.onclick = () => {
   if(showAuthor) {
@@ -23,5 +25,20 @@ btn_toggle.onclick = () => {
   showAuthor = !showAuthor
 }
 
+search_btn.onclick = () => {
+    text = paragraph.innerHTML
+    const l = length.value
+    const colorVal = color.value
+    paragraph.innerHTML = ""
 
+    const words = text.split(" ");
+    words.forEach((word) => {
+      const span = document.createElement("span")
+      span.innerHTML = word + " "
+      if(word.length >= l) {
+        span.style.color = colorVal
+      }
+      paragraph.appendChild(span)
+    })
+}
 // more codes for Search and Reset buttons here
