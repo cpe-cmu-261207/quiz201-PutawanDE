fe312a4b55451aeefbb1b878ddbb34f01248ad8f
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-07-29 14:09:05 +0700

    Finish author section

--- script.js
+++ script.js
@@ -5,9 +5,23 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 // define more constants and variables here
+let showAuthor = false
+
 
 btn_toggle.onclick = () => {
-  calculation.innerHTML = 630610734 + parseInt(length.value);
+  if(showAuthor) {
+    btn_toggle.innerHTML = "Show Calculation"
+    author.style.display = "block"
+    calculation.style.display = "none"
+  } else {
+    author.style.display = "none"
+    calculation.style.display = "block"
+    calculation.innerHTML = 630610734 + parseInt(length.value);
+    btn_toggle.innerHTML = "Show Author"
+    // author.parentNode.removeChild(author)
+  }
+  showAuthor = !showAuthor
 }
+
 
 // more codes for Search and Reset buttons here
